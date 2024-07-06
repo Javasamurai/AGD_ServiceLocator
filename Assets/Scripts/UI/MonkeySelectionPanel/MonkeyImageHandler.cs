8481a9f319621274601a4de33dfebbaac14eeca8
Syed zainul abedin <szsimplesyed93@gmail.com>
2024-07-06 17:07:30 +0530

    Updated drag logic

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,14 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IPointerDownHandler, IEndDragHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private RectTransform rectTransform;
+        private Vector2 initialPosition;
+        private Vector2 initialAchoredPosition;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -18,9 +22,38 @@ namespace ServiceLocator.UI
             this.owner = owner;
         }
 
+        public void OnDrag(PointerEventData eventData)
+        {
+            rectTransform.anchoredPosition += eventData.delta;
+            owner.MonkeyDraggedAt(rectTransform.position);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkey();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyImage.color = new Color(1, 1, 1, 1);
+            rectTransform.anchoredPosition = initialAchoredPosition;
+            rectTransform.position = initialPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1, 1, 1, 0.5f);
+        }
+
         private void Awake()
         {
             monkeyImage = GetComponent<Image>();
+            rectTransform = GetComponent<RectTransform>();
+            initialPosition = rectTransform.position;
+            initialAchoredPosition = rectTransform.anchoredPosition;
             monkeyImage.sprite = spriteToSet;
         }
     }
