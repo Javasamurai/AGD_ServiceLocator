22d2ea59cb4cb105a6cb4176fe096c362fffd55b
Syed zainul abedin <szsimplesyed93@gmail.com>
2024-08-27 20:02:52 +0530

    Added EventService

---
+++ Assets/Scripts/Service/EventService.cs
@@ -0,0 +1,21 @@
+class EventService
+{
+    private static EventService _instance;
+    public static EventService Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new EventService();
+            }
+            return _instance;
+        }
+    }
+
+    public EventController OnLightSwitchToggled { get; private set; }
+    public EventService()
+    {
+        OnLightSwitchToggled = new EventController();
+    }
+}
