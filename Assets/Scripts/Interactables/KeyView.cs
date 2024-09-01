69114e648c74e640300540df509166f74f4ee3f4
Syed zainul abedin <szsimplesyed93@gmail.com>
2024-09-01 18:04:31 +0530

    Added generic eventcontroller

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class KeyView : MonoBehaviour, IInteractable
 {
     [SerializeField] GameUIView gameUIView;
+    private int keysEquipped;
     public void Interact()
     {
         GameService.Instance.GetInstructionView().HideInstruction();
-        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
-        GameService.Instance.GetPlayerController().KeysEquipped++;
+        // GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
+        // GameService.Instance.GetPlayerController().KeysEquipped++;
+        keysEquipped++;
+        EventService.Instance.OnKeyPickedUp.InvokeEvent(keysEquipped);
         gameUIView.UpdateKeyText();
-
         gameObject.SetActive(false);
     }
 }
