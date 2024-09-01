6e699e45a542432b518a291586c3be0fb812b905
Syed zainul abedin <szsimplesyed93@gmail.com>
2024-09-01 23:04:36 +0530

    Ghost trigger logic added

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
-
+    [SerializeField] private int keyRequiredToTrigger;
+    [SerializeField] private SoundType soundType;
+    private void OnTriggerEnter(Collider other) {
+        if (other.GetComponent<PlayerView>() != null && GameService.Instance.GetPlayerController().KeysEquipped == keyRequiredToTrigger) {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
+            this.enabled = false;
+        }
+    }
 }
